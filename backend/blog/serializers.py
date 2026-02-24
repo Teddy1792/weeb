acbc05cb65b5ce2d78787270c6fe3138ad10ad83
Theodore Abitbol <theodore.abitbol@gmail.com>
2026-02-24 19:07:18 +0100

    added article update

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import Post
 
+
 class PostSerializer(serializers.ModelSerializer):
+    author_name = serializers.SerializerMethodField()
+
+    def get_author_name(self, obj):
+        full_name = f"{obj.author.first_name} {obj.author.last_name}".strip()
+        return full_name or obj.author.email
+
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = (
+            "id",
+            "author",
+            "author_name",
+            "title",
+            "content",
+            "created_at",
+            "updated_at",
+        )
         read_only_fields = ("author",)
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
@@ -11,3 +14,15 @@ class PostViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=["post"], url_path="edit")
+    def edit_post(self, request, pk=None):
+        post = self.get_object()
+        serializer = self.get_serializer(
+            post,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
